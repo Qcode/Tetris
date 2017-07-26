43c3442b9821be27e6aa6bf3b726f3fc4cf49308
Ross Evans <therealrossevans@gmail.com>
2017-07-25 22:40:39 -0300

    Add tetromino layouts

--- main.lua
+++ main.lua
@@ -1,7 +1,57 @@
+math.randomseed(os.time())
+for x = 1, 5 do
+    math.random()
+end
+
 local gridWidth = 10
 local gridHeight = 24
 local gridSize = 16
 local grid = {}
+
+local tetrominoes = {
+    {
+        {false, true, false, false},
+        {false, true, false, false},
+        {false, true, false, false},
+        {false, true, false, false}
+    },
+    {
+        {false, true, true, false},
+        {false, true, false, false},
+        {false, true, false, false},
+        {false, true, false, false}
+    },
+    {
+        {false, true, true, false},
+        {false, false, true, false},
+        {false, false, true, false},
+        {false, false, true, false}
+    },
+    {
+        {false, false, false, false},
+        {false, true, true, false},
+        {false, true, true, false},
+        {false, false, false, false}
+    },
+    {
+        {false, false, false, false},
+        {false, true, false, false},
+        {true, true, true, false},
+        {false, false, false, false}
+    },
+    {
+        {false, false, true, true},
+        {false, true, true, false},
+        {false, false, false, false},
+        {false, false, false, false}
+    },
+    {
+        {true, true, false, false},
+        {false, true, true, false},
+        {false, false, false, false},
+        {false, false, false, false}
+    }
+}
 
 function love.load()
     for x = 0, gridWidth - 1 do
@@ -10,6 +60,9 @@ function love.load()
             grid[x][y] = false
         end
     end
+
+    tetromino = newTetromino()
+    print(tetromino.layout[1][1])
 end
 
 function love.update(dt)
@@ -25,8 +78,37 @@ function love.draw()
             love.graphics.rectangle(fillMode, x*gridSize, y*gridSize, gridSize, gridSize)
         end
     end
+
+    for x = 0, 3 do
+        for y = 0, 3 do
+            if tetromino.layout[x+1][y+1] then
+                love.graphics.rectangle(
+                    'fill',
+                    (tetromino.x+x)*gridSize,
+                    (tetromino.y+y)*gridSize,
+                    gridSize,
+                    gridSize
+                )
+            end
+        end
+    end
     love.graphics.pop()
 end
 
 function love.keypressed(key)
+end
+
+function newTetromino()
+    local new = {x = 4, y = -3}
+    local copyTable = {}
+    local tetromino = tetrominoes[math.random(#tetrominoes)]
+    for x = 1, 4 do
+        copyTable[x] = {}
+        for y = 1, 4 do
+            print(x, y, tetromino[x][y])
+            copyTable[x][y] = tetromino[y][x]
+        end
+    end
+    new.layout = copyTable
+    return new
 end
